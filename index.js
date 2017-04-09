a1d82b34599101293c9321428e8c703e118d527a
Ryan Gill <ryanjgill2@gmail.com>
2017-04-08 21:21:07 -0500

    fix johnny-five require, and err on board ready

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
   , path = require('path')
   , http = require('http').createServer(app)
   , socketIO = require('socket.io')(http)
-  , five = require('five')
+  , five = require('johnny-five')
   , os = require('os')
   , eth0 = os.networkInterfaces().apcli0
   , address = eth0 && eth0.length && eth0[0].address
@@ -24,7 +24,7 @@ app.get('/', function () {
 });
 
 // board ready event
-board.on('ready', function () {
+board.on('ready', function (err) {
   if (err) {
     console.log(err);
     board.reset();
